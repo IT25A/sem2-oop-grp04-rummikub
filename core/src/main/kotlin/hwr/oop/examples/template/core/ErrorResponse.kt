db8a0d713111c984f1e4207aa2c7b52b6956de00
Ricardo Scherffig <rickyrabv@gmail.com>
2026-04-28 18:03:55 +0200

    feat: classes added
    # Conflicts:
    #	core/src/test/kotlin/hwr/oop/examples/template/core/ExampleTest.kt
    
    # Conflicts:
    #	Requirements.md
    #	core/src/main/kotlin/hwr/oop/examples/template/core/Game.kt
    #	core/src/main/kotlin/hwr/oop/examples/template/core/Tile.kt
    #	core/src/main/kotlin/hwr/oop/examples/template/core/TileColor.kt

---
+++ core/src/main/kotlin/hwr/oop/examples/template/core/ErrorResponse.kt
@@ -0,0 +1,7 @@
+package hwr.oop.examples.template.core
+
+class ErrorResponse {
+    var status:Int;
+    var error:String;
+    var message:String;
+}
---
+++ core/src/main/kotlin/hwr/oop/examples/template/core/TileSetType.kt
@@ -0,0 +1,6 @@
+package hwr.oop.examples.template.core
+
+enum class TileSetType {
+    "RUN",
+    "GROUP"
+}
