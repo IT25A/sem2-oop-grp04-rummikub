f6740032a9bcf1c45f8679b00f1061560ef097b7
Ricardo Scherffig <ricardo@scherffig.de>
2026-05-02 12:22:56 +0200

    feat: mutable pool class, player rack class, pool class, pool tests, and fillingrackstest

--- core/src/main/kotlin/hwr/oop/examples/template/core/PlayerRack.kt
+++ core/src/main/kotlin/hwr/oop/examples/template/core/PlayerRack.kt
@@ -1,1 +1,9 @@
 package hwr.oop.examples.template.core
+
+data class PlayerRack(
+    private val player: PlayerId,
+    private val tiles: List<Tile>,
+) {
+    fun player(): PlayerId = player
+    fun tiles(): List<Tile> = tiles
+}
