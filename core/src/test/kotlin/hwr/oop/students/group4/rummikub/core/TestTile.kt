5c6b511db3055b1966a9fb2fe815586310c61622
Boas-HWR25 <s_kuellmei25@stud.hwr-berlin.de>
2026-05-06 11:49:29 +0200

    Made requested changes, removed Examle files

--- core/src/test/kotlin/hwr/oop/students/group4/rummikub/core/TestTile.kt
+++ core/src/test/kotlin/hwr/oop/students/group4/rummikub/core/TestTile.kt
@@ -1,45 +1,31 @@
 package hwr.oop.students.group4.rummikub.core
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.MethodSource
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestTile {
+	fun provideTestData(): Iterable<Pair<TileNumber, TileColor>> {
+		val testData = TileNumber.entries.flatMap { number ->
+			TileColor.entries.map {
+				number to it
+			}
+		}
+		return testData.asIterable()
+	}
+	
 	@ParameterizedTest
-	@EnumSource(TileNumber::class)
-	fun testTile(number: TileNumber) {
-		for (color in TileColor.entries) { // please provide Feedback whether it is okay to just use the enum classes, since they are already tested and in authorized changes in sourcecode are already caught by TestTileColor and TestTileNumber
-			// given
-			
-			// when
-			val tile = Tile(number = number, color = color)
-			// then
-			assertThat(tile.number()).isEqualTo(number)
-			assertThat(tile.color()).isEqualTo(color)
+	@MethodSource("provideTestData")
+	fun testTile(tileData: Pair<TileNumber, TileColor>) {
+		// given
+		val number = tileData.first
+		val color = tileData.second
+		// when
+		val tile = Tile(number=number, color = color)
+		// then
+		assertThat(tile.number()).isEqualTo(number)
+		assertThat(tile.color()).isEqualTo(color)
 		}
 	}
-	
-	// is only one combination, should be fine tho right?
-	@Test
-	fun `test toString()`() {
-		// given
-		val instance = Tile(TileColor.RED, TileNumber.THIRTEEN)
-		// when
-		val toStringOutput = instance.toString()
-		// then
-		assertThat(toStringOutput).isEqualTo("Tile(color=RED, number=THIRTEEN)")
-	}
-	
-	@Test
-	fun `test equals()`() {
-		// given
-		val instance1 = Tile(TileColor.RED, TileNumber.ONE)
-		val instance2 = Tile(TileColor.RED, TileNumber.ONE)
-		// when
-		val result = instance1.equals(instance2)
-		// then
-		assertThat(result).isTrue()
-	}
-
-}
