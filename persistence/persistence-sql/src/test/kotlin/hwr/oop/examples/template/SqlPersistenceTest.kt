778ba2a238d2a2863cf42fe49ab400f9279a079b
Eliss433 <s_zawal25@stud.hwr-berlin.de>
2026-07-11 22:17:55 +0200

    test: update sql persistence test for GameId

--- persistence/persistence-sql/src/test/kotlin/hwr/oop/examples/template/SqlPersistenceTest.kt
+++ persistence/persistence-sql/src/test/kotlin/hwr/oop/examples/template/SqlPersistenceTest.kt
@@ -13,6 +13,7 @@ import hwr.oop.students.group4.rummikub.core.GameId
 import hwr.oop.students.group4.rummikub.core.PlayerId
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import hwr.oop.ports.out.LoadGameByIdPort
 
 @Disabled("Requires Docker")
 @Testcontainers
@@ -48,12 +49,15 @@ class SqlPersistenceTest {
 	@Test
 	fun `save game and load game successfully`() {
 		// given
-		val newGame = Game.createNewGame(listOf(PlayerId("player 1"), PlayerId("player 2")))
-		val gameId = newGame.id()
+		val gameId = GameId("1111")
+		val newGame = Game.createNewGame(
+			gameId,
+			listOf(PlayerId("player 1"), PlayerId("player 2"))
+		)
 
 		// when
 		adapter.save(newGame)
-		val loadedGame = adapter.load(gameId)
+		val loadedGame = adapter.loadById(gameId)
 
 		// then
 		assertThat(loadedGame).isEqualTo(newGame)
@@ -65,8 +69,8 @@ class SqlPersistenceTest {
 
 		// when
 		// then
-		assertThatThrownBy { adapter.load(gameId) }
-			.hasMessageContaining("Game not found: $gameId")
+		assertThatThrownBy { adapter.loadById(gameId) }
+			.isInstanceOf(LoadGameByIdPort.CouldNotLoadException::class.java)
 	}
 }
 
